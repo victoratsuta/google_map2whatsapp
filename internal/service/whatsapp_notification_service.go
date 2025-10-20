a316cb9b0122b35952f38bd54c652ed63fca8acb
viktoratsuta <atsutavictor.dev@gmail.com>
2025-10-20 17:34:41 +0200

    fix go rutines flow

--- internal/service/whatsapp_notification_service.go
+++ internal/service/whatsapp_notification_service.go
@@ -4,18 +4,24 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
+	"sync"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/mdp/qrterminal/v3"
 	"github.com/victoratsuta/google_map2whatsapp/internal/entity"
 	"go.mau.fi/whatsmeow"
-	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/proto/waE2E"
 	"go.mau.fi/whatsmeow/store/sqlstore"
 	"go.mau.fi/whatsmeow/types"
 	waLog "go.mau.fi/whatsmeow/util/log"
 )
+
+var wg = &sync.WaitGroup{}
+
+type messageSendReport struct {
+	company string
+	err     error
+}
 
 type WhatsAppNotificationService struct {
 	store     *sqlstore.Container
@@ -39,8 +45,8 @@ func NewWhatsAppNotificationService(
 func (s *WhatsAppNotificationService) Auth() error {
 
 	if s.client.Store.ID == nil {
-		if err := s.handleNewLogin(); err != nil {
-			return fmt.Errorf("login failed: %w", err)
+		if err := s.handleNewQRScanLogin(); err != nil {
+			return fmt.Errorf("login to Whatsapp is failed: %w", err)
 		}
 	}
 
@@ -49,16 +55,54 @@ func (s *WhatsAppNotificationService) Auth() error {
 
 func (s *WhatsAppNotificationService) SendToWhatsApp(companies entity.CompanyCollection, message string) error {
 
-	if err := s.handleExistingSession(companies, message); err != nil {
-		return fmt.Errorf("failed to send messages: %w", err)
+	results := make(chan messageSendReport, len(companies.Get()))
+
+	if !s.client.IsConnected() {
+		if err := s.client.Connect(); err != nil {
+			return fmt.Errorf("failed to connect: %w", err)
+		}
 	}
 
-	s.waitForInterrupt()
+	defer s.client.Disconnect()
+
+	for _, company := range companies.Get() {
+		wg.Add(1)
+		go func() {
+			if err := s.sendText(s.client, message, company.PhoneNumber()); err != nil {
+				results <- messageSendReport{
+					company: company.Name(),
+					err:     fmt.Errorf("Send failed to %s: %v\n", company.Name(), err),
+				}
+			} else {
+				results <- messageSendReport{
+					company: company.Name(),
+					err:     nil,
+				}
+			}
+			wg.Done()
+		}()
+
+	}
+
+	wg.Wait()
+	close(results)
+
+	var errs []error
+	for r := range results {
+		if r.err != nil {
+			errs = append(errs, fmt.Errorf("send to %s: %w", r.company, r.err))
+		}
+	}
+
+	if len(errs) > 0 {
+		return fmt.Errorf("error report during send to whatsapp: %v", errs)
+	}
+
 	return nil
 }
 
-func (s *WhatsAppNotificationService) handleNewLogin() error {
-	qrChan, _ := s.client.GetQRChannel(context.Background())
+func (s *WhatsAppNotificationService) handleNewQRScanLogin() error {
+	qrChan, _ := s.client.GetQRChannel(context.TODO())
 	if err := s.client.Connect(); err != nil {
 		return fmt.Errorf("failed to connect: %w", err)
 	}
@@ -74,42 +118,12 @@ func (s *WhatsAppNotificationService) handleNewLogin() error {
 	return nil
 }
 
-func (s *WhatsAppNotificationService) handleExistingSession(companies entity.CompanyCollection, message string) error {
+func (s *WhatsAppNotificationService) sendText(client *whatsmeow.Client, text string, phoneE164Digits string) error {
 
-	if !s.client.IsConnected() {
-		if err := s.client.Connect(); err != nil {
-			return fmt.Errorf("failed to connect: %w", err)
-		}
-	}
-
-	for _, company := range companies.Get() {
-
-		// Capture company in loop scope for goroutine
-		company := company
-
-		go func() {
-			if err := s.sendText(context.Background(), s.client, message, company.PhoneNumber()); err != nil {
-				fmt.Printf("Send failed to %s: %v\n", company.Name, err)
-			} else {
-				fmt.Printf("Message sent successfully to %s!\n", company.Name)
-			}
-		}()
-
-	}
-
-	return nil
-}
-
-func (s *WhatsAppNotificationService) sendText(ctx context.Context, client *whatsmeow.Client, text string, phoneE164Digits string) error {
-	jid := types.NewJID(phoneE164Digits, types.DefaultUserServer)
-	msg := &waProto.Message{Conversation: &text}
-	_, err := client.SendMessage(ctx, jid, msg)
+	_, err := client.SendMessage(
+		context.TODO(),
+		types.NewJID(phoneE164Digits, types.DefaultUserServer),
+		&waE2E.Message{Conversation: &text},
+	)
 	return err
-}
-
-func (s *WhatsAppNotificationService) waitForInterrupt() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-	s.client.Disconnect()
 }
