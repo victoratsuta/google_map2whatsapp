666c88ac3de6eefbea8dea8f27961e68ffe996b7
viktoratsuta <atsutavictor.dev@gmail.com>
2025-10-20 17:35:32 +0200

    fix CLI interface with colorful inputs + README.MD update

--- cmd/run.go
+++ cmd/run.go
@@ -2,60 +2,86 @@ package cmd
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/victoratsuta/google_map2whatsapp/config"
 	"github.com/victoratsuta/google_map2whatsapp/internal/entity"
 )
 
 func Execute(container *config.Container) {
 
-	fmt.Println("\nThis program send messages through ur Whatsapp from Google Maps by provided location")
-	fmt.Println("\nLets first auth in ur Whatsapp, scan this QR with ur Whatsapp. This step could be skipped if u already logged in")
+	greetings()
+	whatsappAuth(container)
 
-	err := container.GetWhatsAppService().Auth()
-	if err != nil {
-		fmt.Printf("❌ Auth error: %v\n", err)
-		return
-	}
 	company := inputSearchingCompany()
 	location := inputSearchingLocation()
 
-	companies, _ := container.GetCompaniesRepository().GetByLocation(location + ", " + company)
+	companies, err := container.GetCompaniesRepository().GetByLocation(location + ", " + company)
+
+	if err != nil {
+		color.Red("❌ Get from Google maps error: %v\n", err)
+		return
+	}
 
 	printCompaniesDetails(companies)
-	fmt.Printf("Total found companies:  %d\n", companies.Count())
 
 	message := inputMessage()
-	fmt.Println("\nYour input:")
-	fmt.Println(message)
-	fmt.Println("\nStarting sending messages to whatsapp")
+
+	color.Blue("\nStarting sending messages to whatsapp")
 
 	err = container.GetWhatsAppService().SendToWhatsApp(companies, message)
 	if err != nil {
-		fmt.Printf("❌ SendToWhatsApp error: %v\n", err)
+		color.Yellow("\nSome sendings failed.")
+		color.Yellow("❌ SendToWhatsApp error: %v\n", err)
 		return
 	}
+
+	color.Green("\nWe are done!")
+}
+
+func greetings() {
+	color.Green("Hi!")
+	color.Blue("\nThis program send messages through ur Whatsapp from Google Maps by provided location")
+	color.Green("---------------------------")
+}
+
+func whatsappAuth(container *config.Container) {
+
+	color.Blue("Lets first auth in ur Whatsapp, scan this QR with ur Whatsapp. This step will be skipped if u already logged in")
+
+	err := container.GetWhatsAppService().Auth()
+	if err != nil {
+		color.Red("❌ Auth error: %v\n", err)
+	}
+
+	color.Green("Auth in Whatsapp is done!\n")
+	color.Green("---------------------------")
 }
 
 func printCompaniesDetails(companies entity.CompanyCollection) {
+
+	color.Green("Total found %d companies\n", companies.Count())
+	color.Green("Here is list of found companies\n")
+
 	for _, company := range companies.Get() {
-		fmt.Println(company.Name())
-		fmt.Println(company.PhoneNumber())
-		fmt.Println("_________________")
+		color.Yellow(company.Name())
+		color.Yellow(company.PhoneNumber())
+		color.Yellow("_________________")
 	}
+
+	color.Green("---------------------------")
 }
 
 func inputSearchingCompany() string {
 	var searchingCompany = "tour company"
-	fmt.Printf("Enter company type u are interested in (example: %s): ", searchingCompany)
+	color.Blue("Enter company type u are interested in (example: %s): ", searchingCompany)
 
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Error reading input:", err)
+		color.Red("Error reading input:", err)
 		return searchingCompany
 	}
 
@@ -69,12 +95,12 @@ func inputSearchingCompany() string {
 
 func inputSearchingLocation() string {
 	var searchingLocation = "Milan"
-	fmt.Printf("Enter location/city you are interested in (example: %s): ", searchingLocation)
+	color.Blue("Enter location/city you are interested in (example: %s): ", searchingLocation)
 
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("Error reading input:", err)
+		color.Red("Error reading input:", err)
 		return searchingLocation
 	}
 
@@ -89,8 +115,8 @@ func inputSearchingLocation() string {
 func inputMessage() string {
 
 	message := "Hi, how are u?"
-	fmt.Printf("Enter message to send (default: %s):\n", message)
-	fmt.Println("(Press Ctrl+D or Ctrl+Z on Windows to finish)")
+	color.Blue("Enter message to send (default: %s):\n", message)
+	color.Blue("(Press Ctrl+D or Ctrl+Z on Windows to finish)")
 
 	scanner := bufio.NewScanner(os.Stdin)
 	var lines []string
@@ -103,6 +129,8 @@ func inputMessage() string {
 	if input != "" {
 		message = input
 	}
-	return message
 
+	color.Green("---------------------------")
+
+	return message
 }
